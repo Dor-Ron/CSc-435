1d644a0e9300187a1556b97d74a190f8dbe45659
Dor-Ron <dor.rondel@gmail.com>
2018-03-24 15:48:25 -0400

    finished code, need to modularize

---
+++ a4/src/main/java/csc435/moocme/a4/ReqJsonObject.java
@@ -0,0 +1,31 @@
+package csc435.moocme.a4;
+
+class ReqJsonObject {
+    public String title;
+    public String platform;
+    public String institution;
+    public String url;
+    public Boolean free;
+    public int id;
+
+    public ReqJsonObject() {
+        super();
+    }
+
+    public ReqJsonObject(int uid, String titl, String plat, String inst, String uri, Boolean price) {
+        this.id = uid;
+        this.title = titl;
+        this.platform = plat;
+        this.institution = inst;
+        this.url = uri;
+        this.free = price;
+    }   
+
+    public ReqJsonObject(String titl, String plat, String inst, String uri, Boolean price) {
+        this.title = titl;
+        this.platform = plat;
+        this.institution = inst;
+        this.url = uri;
+        this.free = price;
+    }  
+}
